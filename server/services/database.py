7bf08712e1c0ff8bae9c8207471829df3a205a5d
Debian <matteo.fornaini@studio.unibo.it>
2024-11-11 14:30:06 +0000

    Fixed db connection

--- server/services/database.py
+++ server/services/database.py
@@ -2,10 +2,13 @@ from bson import ObjectId
 from motor.motor_asyncio import AsyncIOMotorClient
 from core import config
 
+
 def default_id():
     return str(ObjectId())
 
 # Ensure unique indexes for username and email
+
+
 async def create_indexes():
     await db.users.create_index("username", unique=True)
     await db.users.create_index("email", unique=True)
@@ -14,12 +17,13 @@ async def create_indexes():
 client = None
 db = None
 
+
 def initialize_db_connection():
     global client, db
-    # client = AsyncIOMotorClient(config.MONGODB_URL)
-    client = AsyncIOMotorClient("mongodb://0.0.0.0:27017")
+    client = AsyncIOMotorClient(config.MONGODB_URL)
     db = client.backgammon
     print("Database connection initialized.")
+
 
 def get_db():
     return db
