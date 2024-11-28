c3cd684dab5113c2f4253737291036f597ad7f06
Lorenzo Giarrusso <lorenzo.giarrusso@studio.unibo.it>
2024-11-28 05:12:31 +0000

    Fix class name

--- client/src/views/TournamentView.vue
+++ client/src/views/TournamentView.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="play-human-view">
+	<div class="tournament-view">
 		<div class="bg"></div>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
