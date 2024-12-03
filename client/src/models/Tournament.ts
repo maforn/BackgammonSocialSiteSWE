25bb442267eb875bcafcf957c7c9a1c388c320f5
Mattia Ferrarini <mattiaferrarini22@gmail.com>
2024-12-03 21:11:02 +0100

    Update models

--- client/src/models/Tournament.ts
+++ client/src/models/Tournament.ts
@@ -1,27 +1,47 @@
+class TournamentStats {
+    username: string;
+    wins: number;
+    losses: number;
+    matches: number;
+    constructor(username: string, wins: number, losses: number, matches: number){
+        this.username = username;
+        this.wins = wins;
+        this.losses = losses;
+        this.matches = matches;
+    }
+}
+
 export class Tournament {
     owner: string;
     participants: string[];
+    confirmed_participants: string[];
     open: boolean;
     name: string;
+    type: string;
     status: string;
-    match_ids: string[][];
+    match_ids: string[];
     rounds_to_win: number;
+    stats: TournamentStats[];
 
     constructor(
         owner: string,
         participants: string[],
         open: boolean,
         name: string,
+        type: string,
         status: string,
-        match_ids: string[][],
+        match_ids: string[],
         rounds_to_win: number
     ){
         this.owner = owner;
         this.participants = participants;
+        this.confirmed_participants = [];
         this.open = open;
         this.name = name;
+        this.type = type;
         this.status = status;
         this.match_ids = match_ids;
         this.rounds_to_win = rounds_to_win;
+        this.stats = [];
     }
 };
--- server/models/tournament.py
+++ server/models/tournament.py
@@ -2,16 +2,25 @@ from pydantic import BaseModel, Field
 from typing import List
 from services.database import default_id
 
+
+class TournamentStats(BaseModel):
+    username: str
+    wins: int
+    losses: int
+    matches: int
+
 class Tournament(BaseModel):
     id: str = Field(default_factory=default_id, alias="_id")
     owner: str
+    type: str
     participants: List[str]
     confirmed_participants: List[str]
     open: bool
-    match_ids: List[List[str]]
+    match_ids: List[str]
     name: str
     status: str
     rounds_to_win: int
+    stats: List[TournamentStats]
 
 class CreateTournamentRequest(BaseModel):
     name: str
