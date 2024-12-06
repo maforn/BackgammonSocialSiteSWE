f9c6a6d3bf97985cb8dda5571d30c1e30b8fd60e
Mattia Ferrarini <mattiaferrarini22@gmail.com>
2024-12-06 23:17:37 +0100

    Fix tests

--- server/models/board_configuration.py
+++ server/models/board_configuration.py
@@ -19,30 +19,30 @@ class Point(BaseModel):
 
 # Starting configuration of the board
 DEFAULT_POINTS: List[Point] = [
-    Point(1, 0),  # Point 1
+    Point(0, 2),  # Point 1
     Point(0, 0),  # Point 2
     Point(0, 0),  # Point 3
     Point(0, 0),  # Point 4
     Point(0, 0),  # Point 5
-    Point(0, 0),  # Point 6
+    Point(5, 0),  # Point 6
     Point(0, 0),  # Point 7
-    Point(0, 0),  # Point 8
+    Point(3, 0),  # Point 8
     Point(0, 0),  # Point 9
     Point(0, 0),  # Point 10
     Point(0, 0),  # Point 11
-    Point(0, 0),  # Point 12
-    Point(0, 0),  # Point 13
+    Point(0, 5),  # Point 12
+    Point(5, 0),  # Point 13
     Point(0, 0),  # Point 14
     Point(0, 0),  # Point 15
     Point(0, 0),  # Point 16
-    Point(0, 0),  # Point 17
+    Point(0, 3),  # Point 17
     Point(0, 0),  # Point 18
-    Point(0, 15),  # Point 19
+    Point(0, 5),  # Point 19
     Point(0, 0),  # Point 20
     Point(0, 0),  # Point 21
     Point(0, 0),  # Point 22
     Point(0, 0),  # Point 23
-    Point(0, 0)  # Point 24
+    Point(2, 0)  # Point 24
 ]
 
 
